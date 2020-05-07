74137416f2efee0105ee93ada5f2fa6bbe73433d
Yannic <42739553+YannicEl@users.noreply.github.com>
2020-05-07 14:17:03 +0200

    small fix

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) {}
   ngOnInit(): void {
-    console.log(this.auth.loginUrl);
+    if (sessionStorage.getItem('token')) {
+      this.router.navigate(['/dashboard']);
+    }
   }
 
   onLogin() {
